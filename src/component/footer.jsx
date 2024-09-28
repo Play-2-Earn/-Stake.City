534dcd42326e50ae932a49e3061cd8293a540596
mit prajapatie <mitprajapati544@gmail.com>
2024-09-28 17:18:58 +0100

    Footer

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -2,13 +2,65 @@ import React from "react";
 import mainlogo from '/mainLogo.png'
 import { Link } from "react-router-dom";
 import '../css/header.css'
+import mainLogo from '/mainLogo.png'
 
 const Footer = () => {
     return (
-        <footer className="py-1 mx-6 h-25 border-2 border-red-700">
-            <div>
-
+        <footer className="mt-4">
+            <div className="flex flex-row flex-wrap align-middle justify-start opacity-30 hover:opacity-60 transition-opacity delay-100 cursor-pointer">
+                <img src={mainLogo} alt="Stake_city" className=" h-24 " />
+                <p className="my-auto text-5xl">Stake.city</p>
             </div>
+
+            <div className=" mx-6 flex flex-row flex-wrap justify-evenly align-middle">
+                <div>
+                    <h5 className="border-b mb-3 font-semibold ">Play</h5>
+                    <ul>
+                        <li><Link>What is Stake City?</Link></li>
+                        <li><Link>Play on web</Link></li>
+                        <li><Link>Events</Link></li>
+                        <li><Link>Tutorial</Link></li>
+                        <li><Link>Explore map</Link></li>
+                    </ul>
+                </div>
+                <div>
+                    <h5 className="border-b mb-3 font-semibold ">Economy</h5>
+                    <ul>
+                        <li><Link>How it work?</Link></li>
+                        <li><Link>Whitepaper</Link></li>
+                        <li><Link>$tokename</Link></li>
+                        <li><Link>team</Link></li>
+                    </ul>
+                </div>
+                <div>
+                    <h5 className="border-b mb-3 font-semibold ">Socials</h5>
+                    <ul>
+                        <li><Link>X/Twitter</Link></li>
+                        <li><Link>Discord</Link></li>
+                        <li><Link>Telegram</Link></li>
+                        <li><Link>Instagram</Link></li>
+                        <li><Link>Facebook</Link></li>
+                        <li><Link>Tik-Tok</Link></li>
+                        <li><Link>Reddit</Link></li>
+                    </ul>
+                </div>
+                <div>
+                    <h5 className="border-b mb-3 font-semibold ">Support</h5>
+                    <ul>
+                        <li><Link>Submit Request</Link></li>
+                        <li><Link>Submit Bug</Link></li>
+                    </ul>
+
+                </div>
+                <div>
+                    <h5 className="border-b mb-3 font-semibold ">Legal</h5>
+                    <ul>
+                        <li><Link>Terms and Condition</Link></li>
+                        <li><Link>Privacy and Policy</Link></li>
+                    </ul>
+                </div>
+            </div>
+
         </footer>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 //  @type {import('tailwindcss').Config} 
 module.exports = {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: [
+    './app.jsx',
+    './src/**/*.{jsx,tsx}', // Adjust this path according to your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
