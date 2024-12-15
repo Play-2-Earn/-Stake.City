f71f524d67cd37228d22536cb9d20fd1ffc920f4
qadirkilledar <qadirk2003@gmail.com>
2024-12-15 23:11:04 +0530

    Fix casing issue for welcomepopup

--- frontend/src/component/explore/MapboxMap.jsx
+++ frontend/src/component/explore/MapboxMap.jsx
@@ -5,7 +5,7 @@ import GamifiedTaskPopup from "./starttask";
 import SearchBar from "./searchbar";
 import UserInfo from './UserInfo';
 import WalletInfo from './WalletInfo';
-import WelcomePopup from './welcomepopup';
+import WelcomePopup from './Welcomepopup';
 import Taskbar from './Taskbar';
 import ZoomOutButton from './ZoomOutButton';
 import mapboxgl from 'mapbox-gl';
